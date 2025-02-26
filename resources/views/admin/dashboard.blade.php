8fc4be60f00bc0a3ea898bf7cf0b5a54bc12fe88
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-02-26 13:34:03 +0100

    feat: created basic views - Admin & Vendor

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Admin Dashboard</title>
+</head>
+
+<body>
+    <h1>This is the ADMIN dashboard!</h1>
+</body>
+
+</html>
