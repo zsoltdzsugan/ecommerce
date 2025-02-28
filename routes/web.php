c649fbaf750c4f8bc874baac79ec90304f879fe4
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-02-28 04:38:05 +0100

    refactor: .

--- routes/web.php
+++ routes/web.php
@@ -18,6 +18,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__ . '/auth.php';
-
 Route::get('admin/login', [AdminController::class, 'login'])->name('admin.login');
+
+require __DIR__ . '/auth.php';
