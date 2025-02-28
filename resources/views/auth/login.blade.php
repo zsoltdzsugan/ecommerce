0bca6d6551ba407ddde302281151469848a32f58
zsolt.dzsugan <zsolt.dzsugan@gmail.com>
2025-02-28 04:40:56 +0100

    refactor: inputs & buttons

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -6,14 +6,14 @@
             </h2>
             <p class="mt-2 text-sm text-center text-gray-300/75 max-w">
                 Or
-                <a href="#" class="font-medium text-blue-500 hover:text-blue-500/75">
+                <a href="#" class="font-medium text-indigo-500 hover:text-indigo-600">
                     create an account
                 </a>
             </p>
         </div>
 
         <div class="sm:mx-auto sm:w-full sm:max-w-md">
-            <div class="py-8 px-4 bg-white shadow sm:px-10 sm:rounded-lg dark:bg-gray-800">
+            <div class="py-8 px-4 bg-white sm:px-10 sm:rounded-lg dark:bg-gray-800">
                 <!-- Separator -->
                 <div class="relative mb-4">
                     <div class="flex absolute inset-0 items-center">
@@ -29,9 +29,9 @@
                         <x-input-label for="email" :value="__('Email')"
                             class="block text-sm font-medium text-gray-300" />
                         <div class="mt-1">
-                            <input id="email" name="email" type="email" autocomplete="email"
+                            <x-text-input id="email" name="email" type="email" autocomplete="email"
                                 :value="old('email')" required
-                                class="block relative py-2 px-3 mt-1 w-full placeholder-gray-500 text-gray-900 rounded-md border border-gray-300 appearance-none sm:text-sm dark:text-gray-100 dark:bg-gray-900 dark:border-gray-900 focus:z-10 focus:border-blue-500 focus:ring-blue-500 focus:outline-none"
+                                class="block relative py-2 px-3 mt-1 w-full placeholder-gray-500 text-gray-900 rounded-md border border-gray-300 appearance-none sm:text-sm dark:text-gray-100 dark:bg-gray-900 dark:border-gray-900 focus:z-10 focus:border-indigo-500 focus:ring-indigo-600 focus:outline-none"
                                 placeholder="Enter your email address" />
                             <x-input-error :messages="$errors->get('email')" class="mt-2" />
                         </div>
@@ -43,7 +43,7 @@
                         <div class="mt-1">
                             <input id="password" name="password" type="password" autocomplete="current-password"
                                 required
-                                class="block relative py-2 px-3 mt-1 w-full placeholder-gray-500 text-gray-900 rounded-md border border-gray-900 appearance-none sm:text-sm dark:text-gray-100 dark:bg-gray-900 focus:z-10 focus:border-blue-500 focus:ring-blue-500 focus:outline-none"
+                                class="block relative py-2 px-3 mt-1 w-full placeholder-gray-500 text-gray-900 rounded-md border border-gray-900 appearance-none sm:text-sm dark:text-gray-100 dark:bg-gray-900 focus:z-10 focus:border-indigo-500 focus:ring-indigo-600 focus:outline-none"
                                 placeholder="Enter your password">
                             <x-input-error :messages="$errors->get('password')" class="mt-2" />
                         </div>
@@ -53,7 +53,7 @@
                         <div class="block mt-2">
                             <label for="remember_me" class="inline-flex items-center">
                                 <input id="remember_me" name="remember_me" type="checkbox"
-                                    class="text-blue-500 rounded border-gray-300 shadow-sm dark:bg-gray-900 dark:border-gray-900 focus:ring-blue-500 dark:focus:ring-offset-gray-800">
+                                    class="text-indigo-500 rounded border-gray-300 shadow-sm dark:bg-gray-900 dark:border-gray-900 focus:ring-indigo-600 dark:focus:ring-offset-gray-800">
                                 <span
                                     class="text-sm text-gray-800 dark:text-gray-400 ms-2">{{ __('Remember me') }}</span>
                             </label>
@@ -61,7 +61,7 @@
 
                         <div class="flex justify-end items-center mt-2 text-sm">
                             @if (Route::has('password.request'))
-                                <a class="text-sm text-gray-600 underline rounded-md dark:text-gray-400 hover:text-gray-900 focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none dark:hover:text-gray-300 dark:focus:ring-offset-gray-800"
+                                <a class="text-sm text-gray-600 underline rounded-md dark:text-gray-400 hover:text-gray-900 focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 focus:outline-none dark:hover:text-gray-300 dark:focus:ring-offset-gray-800"
                                     href="{{ route('password.request') }}">
                                     {{ __('Forgot your password?') }}
                                 </a>
@@ -71,7 +71,7 @@
 
                     <div>
                         <button type="submit"
-                            class="flex relative justify-center py-2 px-4 w-full text-sm font-medium text-gray-100 bg-blue-500 rounded-md border border-transparent focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none group hover:bg-blue-500/75">
+                            class="flex relative uppercase tracking-widest justify-center py-2 px-4 w-full text-xs font-semibold text-gray-100 bg-indigo-500 rounded-md border border-transparent focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 focus:outline-none group hover:bg-indigo-600">
                             {{ __('Log in') }}
                         </button>
                     </div>
@@ -83,7 +83,7 @@
                             <div class="w-full border-t border-gray-300"></div>
                         </div>
                         <div class="flex relative justify-center text-sm">
-                            <span class="px-2 text-gray-800 bg-gray-300">
+                            <span class="px-2 text-gray-800 bg-gray-300 rounded-sm">
                                 Or continue with
                             </span>
                         </div>
@@ -92,25 +92,25 @@
                     <div class="grid grid-cols-3 gap-3 mt-6">
                         <div>
                             <a href="#"
-                                class="flex justify-center items-center py-3 px-8 w-full text-sm font-medium text-gray-700 bg-gray-200 rounded-md border border-transparent shadow-sm hover:bg-white focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none group">
+                                class="flex justify-center items-center py-3 px-8 w-full text-sm font-medium text-gray-700 bg-gray-200 rounded-md border border-transparent shadow-sm hover:bg-white focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 focus:outline-none group">
                                 <img class="w-5 h-5"
-                                    src="https://img.icons8.com/?size=100&id=87277&format=png&color=3b82f6"
+                                    src="https://img.icons8.com/?size=100&id=87277&format=png&color=6366f1"
                                     alt="Login with Facebook">
                             </a>
                         </div>
                         <div>
                             <a href="#"
-                                class="flex justify-center items-center py-3 px-8 w-full text-sm font-medium text-gray-700 bg-gray-200 rounded-md border border-transparent shadow-sm hover:bg-white focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none group">
+                                class="flex justify-center items-center py-3 px-8 w-full text-sm font-medium text-gray-700 bg-gray-200 rounded-md border border-transparent shadow-sm hover:bg-white focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 focus:outline-none group">
                                 <img class="w-5 h-5"
-                                    src="https://img.icons8.com/?size=100&id=A4DsujzAX4rw&format=png&color=3b82f6"
+                                    src="https://img.icons8.com/?size=100&id=A4DsujzAX4rw&format=png&color=6366f1"
                                     alt="Login with X">
                             </a>
                         </div>
                         <div>
                             <a href="#"
-                                class="flex justify-center items-center py-3 px-8 w-full text-sm font-medium text-gray-700 bg-gray-200 rounded-md border border-transparent shadow-sm hover:bg-white focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 focus:outline-none group">
+                                class="flex justify-center items-center py-3 px-8 w-full text-sm font-medium text-gray-700 bg-gray-200 rounded-md border border-transparent shadow-sm hover:bg-white focus:ring-2 focus:ring-indigo-600 focus:ring-offset-2 focus:outline-none group">
                                 <img class="w-5 h-5"
-                                    src="https://img.icons8.com/?size=100&id=u8a3cm5LAIui&format=png&color=3b82f6"
+                                    src="https://img.icons8.com/?size=100&id=u8a3cm5LAIui&format=png&color=6366f1"
                                     alt="Login with Google">
                             </a>
                         </div>
